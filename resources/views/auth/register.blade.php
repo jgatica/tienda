00e2300e650651f58ae07b3cf5c42e0d25129332
Jorge Gatica <jgatica@actis.cl>
2020-07-04 12:23:57 -0400

    Se adapta etiqueta form para que apunte al registro. Se agrega el boton submit para registrarse

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
                 <div class="col-lg-4 col-md-6 ml-auto mr-auto">
                     <div class="card card-login">
-                        <form class="form" method="POST" action="{{ route('login') }}">
+                        <form class="form" method="POST" action="{{ route('register') }}">
 
                             @csrf
 
@@ -28,7 +28,7 @@
                             </div>
                             <p class="description text-center">Completa tus datos</p>
                             <div class="card-body">
-                                {{--<div class="input-group">
+                                <div class="input-group">
                                     <div class="input-group-prepend">
                     <span class="input-group-text">
                       <i class="material-icons">face</i>
@@ -36,14 +36,16 @@
                                     </div>
                                     <input id="name" type="name"
                                            class="form-control @error('name') is-invalid @enderror" name="name"
-                                           value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                           value="{{ old('name') }}" required autocomplete="name" autofocus
+                                           placeholder="Nombre..."
+                                    >
 
                                     @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                     @enderror
-                                </div>--}}
+                                </div>
                                 <div class="input-group">
                                     <div class="input-group-prepend">
                     <span class="input-group-text">
@@ -52,7 +54,9 @@
                                     </div>
                                     <input id="email" type="email"
                                            class="form-control @error('email') is-invalid @enderror" name="email"
-                                           value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                           value="{{ old('email') }}" required autocomplete="email" autofocus
+                                           placeholder="Email..."
+                                    >
 
                                     @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -68,7 +72,9 @@
                                     </div>
                                     <input id="password" type="password"
                                            class="form-control @error('password') is-invalid @enderror" name="password"
-                                           required autocomplete="current-password">
+                                           required autocomplete="current-password"
+                                           placeholder="Password"
+                                    >
 
                                     @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -77,20 +83,45 @@
                                     @enderror
                                 </div>
 
-                                <div class="checkbox">
-                                    <label class="form-check-label" for="remember">
-                                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                    <span class="input-group-text">
+                      <i class="material-icons">lock_outline</i>
+                    </span>
+                                    </div>
+                                    <input id="password-confirm" type="password" class="form-control"
+                                           name="password_confirmation" required autocomplete="new-password"
+                                           placeholder="Confirmar password..."
+                                    >
 
-                            <div class="footer text-center">
-                                <a href="#" class="btn btn-primary btn-link btn-wd btn-lg" onclick="this.closest('form').submit();return false;">Confirmar registro !</a>
-                                {{--<button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>--}}
+                                    @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+
+
+                                <div class="input-group ">
+                                    <button type="submit" class="btn btn-simple btn-primary btn-lg ">
+                                        Confirmar registro
+                                    </button>
+                                </div>
+
+
+
                             </div>
+                            {{--<div class="footer text-center">
+                                <button type="submit" class="btn btn-simple btn-primary btn-lg">
+                                    Confirmar registro
+                                </button>
+                            </div>--}}
+
+{{--                            <div class="footer text-center">
+                                <button type="submit" class="btn btn-simple btn-primary btn-lg">
+                                    Confirmar registro
+                                </button>
+                            </div>--}}
                         </form>
                     </div>
                 </div>
