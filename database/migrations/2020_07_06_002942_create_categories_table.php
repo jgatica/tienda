631b429654f38d525406c3db25ac33bab8afb0ed
Jorge Gatica <jgatica@actis.cl>
2020-07-05 20:40:34 -0400

    Se declaran las columnas name, description y image

--- database/migrations/2020_07_06_002942_create_categories_table.php
+++ database/migrations/2020_07_06_002942_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
