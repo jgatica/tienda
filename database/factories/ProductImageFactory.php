3f3a80e97c31b76e08c1f328ebe52ffe491dbc61
Jorge Gatica <jgatica@actis.cl>
2020-07-11 15:08:32 -0400

    Se crea la migracion y el factory para crear tabla product_images

---
+++ database/factories/ProductImageFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\ProductImage::class, function (Faker $faker) {
+    return [
+
+        'image'      => $faker->imageUrl(250, 250),
+        'product_id' => $faker->numberBetween(1, 100)
+    ];
+});
---
+++ database/migrations/2020_07_10_013917_create_product_images_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image');
+            $table->boolean('feature')->default(false);
+            $table->timestamps();
+
+            //FK
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+}
