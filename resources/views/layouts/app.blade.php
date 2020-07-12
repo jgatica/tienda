e9ba5e0ba7d3b155e9b7caa3dfc39614c7c88c99
Jorge Gatica <jgatica@actis.cl>
2020-07-12 14:53:45 -0400

    Se cambia el titulo y se agrega un yield para poder configurar este atributo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,7 @@ The above copyright notice and this permission notice shall be included in all c
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/material-kit//img/apple-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('assets/material-kit//img/favicon.png') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>
-        Figuras de acción
-    </title>
+    <title> @yield('title', 'App Shop')</title>
     <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
@@ -35,8 +33,7 @@ The above copyright notice and this permission notice shall be included in all c
 <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
     <div class="container">
         <div class="navbar-translate">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                Figuras de Acción </a>
+            <a class="navbar-brand" href="{{ url('/') }}">App Shop </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="navbar-toggler-icon"></span>
