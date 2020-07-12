50acaa75986dd8422e6ae7357d268f6848d332e4
Jorge Gatica <jgatica@actis.cl>
2020-07-12 14:46:54 -0400

    Se crea en el seeder una manera de crear Categorias, Productos y Imagenes de productos relacionados

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -13,8 +13,32 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         // Aca se hace uso de los model factories
-        factory(\App\Category::class, 5)->create();
+/*        factory(\App\Category::class, 5)->create();
         factory(Product::class, 100)->create();
-        factory(\App\ProductImage::class, 200)->create();
+        factory(\App\ProductImage::class, 200)->create();*/
+
+        //28. Model factories con relaciones
+/*        $users = factory(App\User::class, 3)
+            ->create()
+            ->each(function ($user) {
+                $user->posts()->save(factory(App\Post::class)->make());
+            });*/
+
+        // Factories con relaciones
+        $categorias =  factory(\App\Category::class, 5)->create();
+        $categorias->each(function ($c){
+            $productos= factory(Product::class, 20)->make();
+            $c->products()->saveMany($productos);
+
+            $productos->each(function ($p) {
+                $imagenes = factory(\App\ProductImage::class, 5)->make();
+                $p->images()->saveMany($imagenes);
+            });
+        });
+/*        $users = factory(App\User::class, 3)
+            ->create()
+            ->each(function ($user) {
+                $user->posts()->save(factory(App\Post::class)->make());
+            });*/
     }
 }
