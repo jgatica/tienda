d8c7e3bc0c0037fe35420d08d0c1831e4a0d2fc3
Jorge Gatica <jgatica@actis.cl>
2020-07-12 19:04:51 -0400

    Se agrega tabla que muestra los productos

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
     {{--header para landing--}}
-    <div class="page-header header-filter" data-parallax="true" style="background-image: url('../assets/material-kit/img/profile_city.jpg')">
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url({{ asset('assets/material-kit/img/profile_city.jpg') }})">
 
     </div>
     {{--header para landing--}}
@@ -24,11 +24,11 @@
                             <thead>
                             <tr>
                                 <th class="text-center">#</th>
-                                <th>Nombre</th>
+                                <th >Nombre</th>
                                 <th>Descripcion</th>
                                 <th>Categoria</th>
-                                <th class="text-right">Precio</th>
-                                <th class="text-right">Acciones</th>
+                                <th>Precio</th>
+                                <th class="w-25">Acciones</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -54,110 +54,8 @@
                             @endforeach
                             </tbody>
                         </table>
-                        @foreach($productos as $product)
-                        <div class="col-md-4">
-                            <div class="team-player">
-                                <div class="card card-plain">
-                                    <div class="col-md-6 ml-auto mr-auto">
 
-                                        <img src="{{ $product->images()->first()->image }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
-                                    </div>
-                                    <h4 class="card-title">{{ $product->name }}
-                                        <br>
-                                        <small class="card-description text-muted">{{ $product->category->name }}</small>
-                                    </h4>
-                                    <div class="card-body">
-                                        <p class="card-description">
-                                            {{ $product->description }}
-                                        </p>
-                                    </div>
-                                    <div class="card-footer justify-content-center">
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
-                        <div class="col-md-4">
-                            <div class="team-player">
-                                <div class="card card-plain">
-                                    <div class="col-md-6 ml-auto mr-auto">
-                                        <img src="{{ asset('assets/material-kit/img/faces/christian.jpg') }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
-                                    </div>
-                                    <h4 class="card-title">Christian Louboutin
-                                        <br>
-                                        <small class="card-description text-muted">Designer</small>
-                                    </h4>
-                                    <div class="card-body">
-                                        <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
-                                            <a href="#">links</a> for people to be able to follow them outside the site.</p>
-                                    </div>
-                                    <div class="card-footer justify-content-center">
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-linkedin"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="team-player">
-                                <div class="card card-plain">
-                                    <div class="col-md-6 ml-auto mr-auto">
-                                        <img src="{{ asset('assets/material-kit/img/faces/kendall.jpg') }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
-                                    </div>
-                                    <h4 class="card-title">Kendall Jenner
-                                        <br>
-                                        <small class="card-description text-muted">Model</small>
-                                    </h4>
-                                    <div class="card-body">
-                                        <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
-                                            <a href="#">links</a> for people to be able to follow them outside the site.</p>
-                                    </div>
-                                    <div class="card-footer justify-content-center">
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
-                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="section section-contacts">
-                <div class="row">
-                    <div class="col-md-8 ml-auto mr-auto">
-                        <h2 class="text-center title">¿Aún no te has registrado?</h2>
-                        <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
-                        <form class="contact-form">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="bmd-label-floating">Nombre</label>
-                                        <input type="email" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="bmd-label-floating">Correo electronico</label>
-                                        <input type="email" class="form-control">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleMessage" class="bmd-label-floating">Tu mensaje</label>
-                                <textarea type="email" class="form-control" rows="4" id="exampleMessage"></textarea>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4 ml-auto mr-auto text-center">
-                                    <button class="btn btn-primary btn-raised">
-                                        Enviar tu consulta
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+
                     </div>
                 </div>
             </div>
