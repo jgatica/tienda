5cb48358d8c52e1db33938800437db0e1b4f87ea
Jorge Gatica <jgatica@actis.cl>
2020-07-12 16:45:17 -0400

    Se apunta a la vista create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,7 +15,7 @@ class ProductController extends Controller
 
     public function create()
     {
-        view('admin.products.index');
+        view('admin.products.create');
     }
 
     public function store()
---
+++ resources/views/admin/products/create.blade.php
@@ -0,0 +1,220 @@
+@extends('layouts.app')
+
+@section('title', 'Bienvenido a App Shop')
+
+@section('body-class', 'landing-page sidebar-collapse')
+
+@section('content')
+    {{--header para landing--}}
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url('../assets/material-kit/img/profile_city.jpg')">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1 class="title">Bienvenidos a App Shop</h1>
+                    <h4>Realiza pedidos en linea y te contactaremos para coordinar la entrega</h4>
+                    <br>
+                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg">
+                        <i class="fa fa-play"></i> Como funciona
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--header para landing--}}
+
+    {{--contenido para el landing--}}
+    <div class="main main-raised">
+        <div class="container">
+            <div class="section text-center">
+                <div class="row">
+                    <div class="col-md-8 ml-auto mr-auto">
+                        <h2 class="title">Hablemos de producto</h2>
+                        <h5 class="description">Puedes revisar nuestra relación completa de productos, comparar precios y realizar tus pedidos cuando estes seguro</h5>
+                    </div>
+                </div>
+                <div class="features">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="info">
+                                <div class="icon icon-info">
+                                    <i class="material-icons">chat</i>
+                                </div>
+                                <h4 class="info-title">Atendemos tus dudas</h4>
+                                <p>Atendemos rápidamente cualquier consulta que tengas via chat. No estas solo, sino que siempre estamos atentos a tus inquietudes.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info">
+                                <div class="icon icon-success">
+                                    <i class="material-icons">verified_user</i>
+                                </div>
+                                <h4 class="info-title">Pago seguro</h4>
+                                <p>Todo pedido que realices sera confirmado a traves de una llamada. Si no confias en los pagos en linea puedes pagar contra entrega el valor acordado</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="info">
+                                <div class="icon icon-danger">
+                                    <i class="material-icons">fingerprint</i>
+                                </div>
+                                <h4 class="info-title">Informacion privada</h4>
+                                <p>Los pedidos que realices solo los conocerás tu a través de tu panel de usuario. Nadie mas tiene acceso a esta información</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section text-center">
+                <h2 class="title">Productos disponibles</h2>
+                <div class="team">
+                    <div class="row">
+                        @foreach($products as $product)
+                        <div class="col-md-4">
+                            <div class="team-player">
+                                <div class="card card-plain">
+                                    <div class="col-md-6 ml-auto mr-auto">
+
+                                        <img src="{{ $product->images()->first()->image }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                    </div>
+                                    <h4 class="card-title">{{ $product->name }}
+                                        <br>
+                                        <small class="card-description text-muted">{{ $product->category->name }}</small>
+                                    </h4>
+                                    <div class="card-body">
+                                        <p class="card-description">
+                                            {{ $product->description }}
+                                        </p>
+                                    </div>
+                                    <div class="card-footer justify-content-center">
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        <div class="col-md-4">
+                            <div class="team-player">
+                                <div class="card card-plain">
+                                    <div class="col-md-6 ml-auto mr-auto">
+                                        <img src="{{ asset('assets/material-kit/img/faces/christian.jpg') }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                    </div>
+                                    <h4 class="card-title">Christian Louboutin
+                                        <br>
+                                        <small class="card-description text-muted">Designer</small>
+                                    </h4>
+                                    <div class="card-body">
+                                        <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
+                                            <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                                    </div>
+                                    <div class="card-footer justify-content-center">
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-linkedin"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="team-player">
+                                <div class="card card-plain">
+                                    <div class="col-md-6 ml-auto mr-auto">
+                                        <img src="{{ asset('assets/material-kit/img/faces/kendall.jpg') }}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                                    </div>
+                                    <h4 class="card-title">Kendall Jenner
+                                        <br>
+                                        <small class="card-description text-muted">Model</small>
+                                    </h4>
+                                    <div class="card-body">
+                                        <p class="card-description">You can write here details about one of your team members. You can give more details about what they do. Feel free to add some
+                                            <a href="#">links</a> for people to be able to follow them outside the site.</p>
+                                    </div>
+                                    <div class="card-footer justify-content-center">
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-twitter"></i></a>
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-instagram"></i></a>
+                                        <a href="#pablo" class="btn btn-link btn-just-icon"><i class="fa fa-facebook-square"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section section-contacts">
+                <div class="row">
+                    <div class="col-md-8 ml-auto mr-auto">
+                        <h2 class="text-center title">¿Aún no te has registrado?</h2>
+                        <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
+                        <form class="contact-form">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="bmd-label-floating">Nombre</label>
+                                        <input type="email" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label class="bmd-label-floating">Correo electronico</label>
+                                        <input type="email" class="form-control">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleMessage" class="bmd-label-floating">Tu mensaje</label>
+                                <textarea type="email" class="form-control" rows="4" id="exampleMessage"></textarea>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4 ml-auto mr-auto text-center">
+                                    <button class="btn btn-primary btn-raised">
+                                        Enviar tu consulta
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{--contenido para el landing--}}
+
+
+    {{--footer para landing--}}
+    <footer class="footer footer-default">
+        <div class="container">
+            <nav class="float-left">
+                <ul>
+                    <li>
+                        <a href="https://www.creative-tim.com/">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/presentation">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/blog">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/license">
+                            Licenses
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright float-right">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>, made with <i class="material-icons">favorite</i> by
+                <a href="https://www.creative-tim.com/" target="_blank">Creative Tim</a> for a better web.
+            </div>
+        </div>
+    </footer>
+    {{--footer para landing--}}
+@endsection
