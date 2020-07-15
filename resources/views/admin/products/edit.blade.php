fcbe5084cabb60c2f1c5e4d9f2d8b32f3ec263be
Jorge Gatica <jgatica@actis.cl>
2020-07-14 22:29:46 -0400

    Se crea la base inicial de la plantilla para editar producto

---
+++ resources/views/admin/products/edit.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('title', 'Bienvenido a App Shop')
+
+@section('body-class', 'profile-page sidebar-collapse')
+
+@section('content')
+    {{--header para landing--}}
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url({{ asset('assets/material-kit/img/profile_city.jpg') }})">
+        <div class="container">
+            <div class="row">
+            </div>
+        </div>
+    </div>
+    {{--header para landing--}}
+
+    {{--contenido para el landing--}}
+    <div class="main main-raised">
+        <div class="container">
+            <div class="section">
+                <h2 class="title text-center">Registrar nuevo producto</h2>
+                <form method="post" action="{{ url('/admin/products') }}" >
+                    @csrf
+             {{--       $table->string('name');
+                    $table->string('description');
+                    $table->text('long_description')->nullable();
+                    $table->float('price');--}}
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group bmd-form-group">
+                                <label class="bmd-label-floating">Nombre del producto</label>
+                                <input type="text" name="name" class="form-control">
+                            </div>
+                            <div class="form-group bmd-form-group">
+                                <label class="bmd-label-floating">Descripción corta</label>
+                                <input type="text" name="description" class="form-control">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group bmd-form-group">
+                        <label class="bmd-label-floating">Precio</label>
+                        <input type="number" name="precio" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Descripción extensa del producto</label>
+                        <textarea class="form-control" name="long_description" id="long_description" rows="3" ></textarea>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">Registrar producto</button>
+
+                </form>
+            </div>
+
+        </div>
+    </div>
+    {{--contenido para el landing--}}
+
+
+    {{--footer para landing--}}
+    <footer class="footer footer-default">
+        <div class="container">
+            <nav class="float-left">
+                <ul>
+                    <li>
+                        <a href="https://www.creative-tim.com/">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/presentation">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/blog">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/license">
+                            Licenses
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright float-right">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>, made with <i class="material-icons">favorite</i> by
+                <a href="https://www.creative-tim.com/" target="_blank">Creative Tim</a> for a better web.
+            </div>
+        </div>
+    </footer>
+    {{--footer para landing--}}
+@endsection
