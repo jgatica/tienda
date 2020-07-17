c33b4c0f37c0a755a8bf56662825a8431bd44b9e
Jorge Gatica <jgatica@actis.cl>
2020-07-17 19:05:57 -0400

    Se agrega un middleware a un grupo de rutas, en realidad son dos middleware. Esto es utilizando https://laravel.com/docs/5.8/routing#route-group-middleware, ya que son 3 formas en las que podemos agrupar las rutas con middlewares

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,15 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Administracion de productos
-Route::get('/admin/products', 'ProductController@index');
-Route::get('/admin/products/create', 'ProductController@create');
-Route::post('/admin/products', 'ProductController@store');
-Route::get('/admin/products/{id}/edit', 'ProductController@edit');
-Route::post('/admin/products/{id}/edit', 'ProductController@update');
-Route::delete('/admin/products/{id}/delete', 'ProductController@destroy');
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    // Administracion de productos
+    Route::get('/admin/products', 'ProductController@index');
+    Route::get('/admin/products/create', 'ProductController@create');
+    Route::post('/admin/products', 'ProductController@store');
+    Route::get('/admin/products/{id}/edit', 'ProductController@edit');
+    Route::post('/admin/products/{id}/edit', 'ProductController@update');
+    Route::delete('/admin/products/{id}/delete', 'ProductController@destroy');
+});
