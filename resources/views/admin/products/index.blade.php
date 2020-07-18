5abfe0bf05faecb83f652767f7d9e5d4e8e899c8
Jorge Gatica <jgatica@actis.cl>
2020-07-18 17:45:40 -0400

    Se agnade boton en index del producto para despues gestionar las imagenes

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -51,6 +51,9 @@
                                         <a href="{{ url('/admin/products/'.$producto->id.'/edit') }}" rel="tooltip" title="Edit Profile" class="btn btn-success btn-simple btn-xs">
                                             <i class="fa fa-edit"></i>
                                         </a>
+                                        <a href="{{ url('/admin/products/'.$producto->id.'/edit') }}" rel="tooltip" title="Imágenes del producto" class="btn btn-warning btn-simple btn-xs">
+                                            <i class="fa fa-image"></i>
+                                        </a>
                                         <button type="submit" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs">
                                             <i class="fa fa-times"></i>
                                         </button>
