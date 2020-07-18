1dab2afb6f227073f079a7f834b3403002b7cd4d
Jorge Gatica <jgatica@actis.cl>
2020-07-18 16:43:31 -0400

    Se sustituye el contenido de la visata home resources/views/home.blade.php por la de create y se le saca el formulario y se deja una pequeña porsion del codigo anterior (lo que estaba en el if)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,81 @@
 @extends('layouts.app')
 
+@section('title', 'App Shop | Dashboard')
+
+@section('body-class', 'profile-page sidebar-collapse')
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    {{--header para landing--}}
+    <div class="page-header header-filter" data-parallax="true" style="background-image: url({{ asset('assets/material-kit/img/profile_city.jpg') }})">
+        <div class="container">
+            <div class="row">
+                {{--                <div class="col-md-6">
+                                    <h1 class="title">Bienvenidos a App Shop</h1>
+                                    <h4>Realiza pedidos en linea y te contactaremos para coordinar la entrega</h4>
+                                    <br>
+                                    <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg">
+                                        <i class="fa fa-play"></i> Como funciona
+                                    </a>
+                                </div>--}}
             </div>
         </div>
     </div>
-</div>
+    {{--header para landing--}}
+
+    {{--contenido para el landing--}}
+    <div class="main main-raised">
+        <div class="container">
+            <div class="section">
+                <h2 class="title text-center">Dashboad</h2>
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                You are logged in!
+            </div>
+
+        </div>
+    </div>
+    {{--contenido para el landing--}}
+
+
+    {{--footer para landing--}}
+    <footer class="footer footer-default">
+        <div class="container">
+            <nav class="float-left">
+                <ul>
+                    <li>
+                        <a href="https://www.creative-tim.com/">
+                            Creative Tim
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/presentation">
+                            About Us
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/blog">
+                            Blog
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.creative-tim.com/license">
+                            Licenses
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="copyright float-right">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script>, made with <i class="material-icons">favorite</i> by
+                <a href="https://www.creative-tim.com/" target="_blank">Creative Tim</a> for a better web.
+            </div>
+        </div>
+    </footer>
+    {{--footer para landing--}}
 @endsection
