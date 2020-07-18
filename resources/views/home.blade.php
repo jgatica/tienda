038fcf408a6d248a642a2ff60102d533678b0ba6
Jorge Gatica <jgatica@actis.cl>
2020-07-18 17:04:10 -0400

    Se agrega de forma estatica y utilizando la documentacion https://demos.creative-tim.com/material-kit/index.html trozo de codigo llamado Navigation Pills que es como navegacion por tabs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,7 +24,7 @@
 
     {{--contenido para el landing--}}
     <div class="main main-raised">
-        <div class="container">
+        <div class="containerSeVeMuyChico">
             <div class="section">
                 <h2 class="title text-center">Dashboad</h2>
                 @if (session('status'))
@@ -33,7 +33,46 @@
                     </div>
                 @endif
 
-                You are logged in!
+                <div class="col-lg-6 col-md-8">
+                    <ul class="nav nav-pills nav-pills-icons" role="tablist">
+                        <!--
+                                        color-classes: "nav-pills-primary", "nav-pills-info", "nav-pills-success", "nav-pills-warning","nav-pills-danger"
+                                    -->
+                        <li class="nav-item">
+                            <a class="nav-link active show" href="#dashboard-1" role="tab" data-toggle="tab" aria-selected="true">
+                                <i class="material-icons">dashboard</i>
+                                Carrito de compras
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#schedule-1" role="tab" data-toggle="tab" aria-selected="false">
+                                <i class="material-icons">schedule</i>
+                                Pedidos realizados
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#tasks-1" role="tab" data-toggle="tab" aria-selected="false">
+                                <i class="material-icons">list</i>
+                                Tasks
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="tab-content tab-space">
+                        <div class="tab-pane active show" id="dashboard-1">
+                            Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits.
+                            <br><br>
+                            Dramatically visualize customer directed convergence without revolutionary ROI.
+                        </div>
+                        <div class="tab-pane" id="schedule-1">
+                            Efficiently unleash cross-media information without cross-media value. Quickly maximize timely deliverables for real-time schemas.
+                            <br><br>Dramatically maintain clicks-and-mortar solutions without functional solutions.
+                        </div>
+                        <div class="tab-pane" id="tasks-1">
+                            Completely synergize resource taxing relationships via premier niche markets. Professionally cultivate one-to-one customer service with robust ideas.
+                            <br><br>Dynamically innovate resource-leveling customer service for state of the art customer service.
+                        </div>
+                    </div>
+                </div>
             </div>
 
         </div>
