d561dfcefd9ddd3a5c5e2b67d4fd69b852bcfcf1
Jorge Gatica <jgatica@actis.cl>
2020-07-18 18:12:20 -0400

    Se crea carpeta exclusiva para gestionar las imagenes y se crea vista index para el listado

--- resources/views/admin/products/images/index.blade.php
+++ resources/views/admin/products/images/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Bienvenido a App Shop')
+@section('title', 'Imágenes de producto')
 
 @section('body-class', 'profile-page sidebar-collapse')
 
@@ -16,13 +16,13 @@
         <div class="container">
 
             <div class="section text-center">
-                <h2 class="title">Productos disponibles</h2>
+                <h2 class="title">Imágenes de producto seleccionado</h2>
                 <div class="team">
                     <div class="row">
 
-                        <a href="{{ url('/admin/products/create') }}" class="btn btn-primary btn-round">Nuevo producto</a>
+                        <a href="{{ url('/admin/products/create') }}" class="btn btn-primary btn-round">Subir nueva imagen</a>
 
-                        <table class="table">
+                        {{--<table class="table">
                             <thead>
                             <tr>
                                 <th class="text-center">#</th>
@@ -51,7 +51,7 @@
                                         <a href="{{ url('/admin/products/'.$producto->id.'/edit') }}" rel="tooltip" title="Edit Profile" class="btn btn-success btn-simple btn-xs">
                                             <i class="fa fa-edit"></i>
                                         </a>
-                                        <a href="{{ url('/admin/products/'.$producto->id.'/images') }}" rel="tooltip" title="Imágenes del producto" class="btn btn-warning btn-simple btn-xs">
+                                        <a href="{{ url('/admin/products/'.$producto->id.'/edit') }}" rel="tooltip" title="Imágenes del producto" class="btn btn-warning btn-simple btn-xs">
                                             <i class="fa fa-image"></i>
                                         </a>
                                         <button type="submit" rel="tooltip" title="Eliminar" class="btn btn-danger btn-simple btn-xs">
@@ -62,8 +62,8 @@
                             </tr>
                             @endforeach
                             </tbody>
-                        </table>
-                        {{ $productos->links() }}
+                        </table>--}}
+                        {{--{{ $productos->links() }}--}}
 
                     </div>
                 </div>
