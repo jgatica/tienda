9bd430ba7ede2bcd640f2d729ae117925c61c76c
Jorge Gatica <jgatica@actis.cl>
2020-07-18 17:22:31 -0400

    Se separa el codigo del footer y se guarda como archivo el cual es incluido con la directiva @include en las demas vistas

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,37 @@
+{{--footer para landing--}}
+<footer class="footer footer-default">
+    <div class="container">
+        <nav class="float-left">
+            <ul>
+                <li>
+                    <a href="http://www.google.cl/">
+                        ¿Necesitas ayuda con un proyecto?
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.google.cl/presentation">
+                        Asesoría personalizada
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.google.cl//blog">
+                        Blog
+                    </a>
+                </li>
+                <li>
+                    <a href="http://www.google.cl//license">
+                        Tutoriales sobre desarrollo web y móvil
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="https://www.creative-tim.com/" target="_blank">Creative Tim</a> for a better web.
+        </div>
+    </div>
+</footer>
+{{--footer para landing--}}
