3d01d79eacfd58de5d451cf1b0cf04a394190c30
Jorge Gatica <jgatica@actis.cl>
2020-07-18 18:10:51 -0400

    Se completa la funcion index y se agregan las funciones store y destroy (sin logica aun)

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
 {
-    //
+    public function index($id)
+    {
+        $producto = Product::find($id);
+        $imagenes = $producto->images;
+        return view('admin.products.images.index', compact('imagenes'));
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
 }
