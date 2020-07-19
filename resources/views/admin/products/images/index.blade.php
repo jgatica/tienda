6e773bf517fc6a5c6ef34e283c27263df52c6726
Jorge Gatica <jgatica@actis.cl>
2020-07-19 14:18:49 -0400

    En el listado de productos se agrega funcionalidad a boton para eliminar imagen

--- resources/views/admin/products/images/index.blade.php
+++ resources/views/admin/products/images/index.blade.php
@@ -34,7 +34,13 @@
                                 <div class="card-body">
                                     <h5 class="card-title">Card title</h5>
                                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <button type="submit" class="btn btn-danger btn-round">Eliminar imagen</button>
+
+                                    <form action="" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="hidden" name="image_id" value="{{ $imagen->id }}">
+                                        <button type="submit" class="btn btn-danger btn-round">Eliminar imagen</button>
+                                    </form>
                                 </div>
                                 <div class="card-footer">
                                     <small class="text-muted">Last updated 3 mins ago</small>
