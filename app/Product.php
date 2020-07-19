56530f8c730965eb59dfb05a418129f6b8bbd974
Jorge Gatica <jgatica@actis.cl>
2020-07-19 15:31:39 -0400

    Se crea funcion accesor para propiedad calculada, que se esta utilizando en welcome

--- app/Product.php
+++ app/Product.php
@@ -15,4 +15,19 @@ class Product extends Model
     {
         return $this->hasMany(ProductImage::class);
     }
+
+    // Asi se llama en la vista feature_image_url asi que en la funcion la pongo con camelcase
+    public function getFeaturedImageUrlAttribute()
+    {
+        $featuredImage = $this->images()->where('featured', true)->first();
+        if (!$featuredImage) {
+            $featuredImage = $this->images()->first();
+        }
+
+        if ($featuredImage) {
+            return $featuredImage->url;
+        }
+
+        return '/images/products/default.gif';
+    }
 }
