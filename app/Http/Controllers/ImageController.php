b16857518207af95752c901932cdab025b7c972c
Jorge Gatica <jgatica@actis.cl>
2020-07-18 20:59:46 -0400

    Se crea logica en metodo store para subir imagen y guardar su ruta en la base de datos

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\ProductImage;
 use Illuminate\Http\Request;
 
 class ImageController extends Controller
@@ -14,9 +15,26 @@ class ImageController extends Controller
         return view('admin.products.images.index', compact('producto','imagenes'));
     }
 
-    public function store()
+    /**
+     * @param uniqid esta funcion devuelve una sequencia de numeros en base a la hora del sistema
+     * funcion getClientOriginalExtension obtiene el nombre original del archivo
+     */
+    public function store(Request $request, $id)
     {
+        // Guardar la imagen en nuestro proyecto
+        $file = $request->file('photo');
+        $path = public_path() . '/images/products';
+        $fileName = uniqid() . $file->getClientOriginalName();
+        $file->move($path, $fileName);
 
+        // Crear 1 registro en la tabla product_images
+        $productImage = new ProductImage();
+        $productImage->image = $fileName;
+        $productImage->feature = false;
+        $productImage->product_id = $id;
+        $productImage->save();
+
+        return back();
     }
 
     public function destroy()
