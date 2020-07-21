d9aaa23302191e06664b4937d4820b89a8fe1862
Jorge Gatica <jgatica@actis.cl>
2020-07-20 21:22:53 -0400

    Se agrega un namespace al grupo de ruta para administracion, de esta forma no es necesario anteponer Admin\ a cada ruta del controlador

--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,19 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/products/{id}', 'ProductController@show');
 
 
-Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+Route::middleware(['auth', 'admin'])->prefix('admin')->namespace('Admin')->group(function () {
 
     // Administracion de productos
-    Route::get('/products', 'Admin\ProductController@index');
-    Route::get('/products/create', 'Admin\ProductController@create');
-    Route::post('/products', 'Admin\ProductController@store');
-    Route::get('/products/{id}/edit', 'Admin\ProductController@edit');
-    Route::post('/products/{id}/edit', 'Admin\ProductController@update');
-    Route::delete('/products/{id}/delete', 'Admin\ProductController@destroy');
+    Route::get('/products', 'ProductController@index');
+    Route::get('/products/create', 'ProductController@create');
+    Route::post('/products', 'ProductController@store');
+    Route::get('/products/{id}/edit', 'ProductController@edit');
+    Route::post('/products/{id}/edit', 'ProductController@update');
+    Route::delete('/products/{id}/delete', 'ProductController@destroy');
 
-    Route::get('/products/{id}/images', 'Admin\ImageController@index');
-    Route::post('/products/{id}/images', 'Admin\ImageController@store');
-    Route::delete('/products/{id}/images', 'Admin\ImageController@destroy');
-    Route::get('/products/{id}/images/select/{image}', 'Admin\ImageController@select'); // Destacar imagen
+    Route::get('/products/{id}/images', 'ImageController@index');
+    Route::post('/products/{id}/images', 'ImageController@store');
+    Route::delete('/products/{id}/images', 'ImageController@destroy');
+    Route::get('/products/{id}/images/select/{image}', 'ImageController@select'); // Destacar imagen
 
 });
