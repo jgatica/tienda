9edad50b868af5a37b6cc295aed824f2777800c7
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-01 21:28:50 -0400

    Se agrega ruta para almacenar el producto agregado al carrito

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/products/{id}', 'ProductController@show');
 
+Route::post('/cart', 'CartDetailController@store');
+
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->namespace('Admin')->group(function () {
 
