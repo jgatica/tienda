9a00af3c5c932ade03d33939b8a9c70817d7ffa4
Jorge Gatica <jorge.gatica@gmail.com>
2020-08-01 21:24:08 -0400

    Se modifica migraciones para las tablas cart y cart_details

--- database/migrations/2020_07_19_202703_create_cart_details_table.php
+++ database/migrations/2020_07_19_202703_create_cart_details_table.php
@@ -16,7 +16,7 @@ class CreateCartDetailsTable extends Migration
         Schema::create('cart_details', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->integer('quantity');
-            $table->integer('discount'); // sera en porcentaje (% int)
+            $table->integer('discount')->default(0); // sera en porcentaje (% int)
             $table->timestamps();
 
             // FK carts
